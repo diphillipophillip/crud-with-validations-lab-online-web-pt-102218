852c299184eb17d67291a5378e1218a40d4e838c
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-10 00:53:12 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,4 @@
-class Song < ActiveRecord::Base 
+ class Song < ActiveRecord::Base 
 validates :title, presence: true
 validates :released, inclusion: { in: [true, false] } 
 validates :title, uniqueness: {scope: :release_year} 
